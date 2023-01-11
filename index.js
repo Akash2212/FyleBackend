000d731509d00b7dd82a699326ca7a8bbccb4dca
unknown <akashrobo2@gmail.com>
2023-01-11 18:48:38 +0530

    Created name endpoint

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const http = require('http');
 const axios = require('axios');
 const express = require('express')
 const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/hello',(req,res)=> {
-    res.send("Hello world");
-})
 
-app.post('/data',(req,res)=>{
-    res.send('post data')
-    console.log(req.data)
-    console.log(res.data)
-})
-
-app.listen(port,hostname,()=>{
+axios.get('https://api.github.com/users/Akash2212')
+.then((r)=>{
+    console.log(r.data.name)
+    app.get('/name',(req,res)=>{
+        res.send(r.data.name);
+    })
+    app.listen(port,hostname,()=>{
     console.log("Server is running")
 })
+})
+.catch((err)=>console.log(err))
+
