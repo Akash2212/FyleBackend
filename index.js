4f40b651ab0ba49450f8c5fb9c360554d01d133c
unknown <akashrobo2@gmail.com>
2023-01-11 19:56:11 +0530

    Added landing endpoint

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 const express = require('express')
 const app = express()
-const hostname = 'fylebackend.netlify.app';
+const hostname = 'fyle.cyclic.app';
 const port = 3000;
 
 
 axios.get('https://api.github.com/users/Akash2212')
 .then((r)=>{
     console.log(r.data.name)
+    app.get('/',(req,res)=>{
+        res.send("Express server")
+    })
     app.get('/name',(req,res)=>{
         res.send(r.data.name);
     })
